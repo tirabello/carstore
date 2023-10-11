462c7fd44936dea31271d12df871d3feb6a09f6f
tirabello <tiago.rabellobraz@gmail.com>
2023-10-11 20:20:20 -0300

    Lab 4 concluido

--- carstore/src/main/java/br/com/carstore/dao/CarDao.java
+++ carstore/src/main/java/br/com/carstore/dao/CarDao.java
@@ -36,6 +36,31 @@ public class CarDao {
 
         }
     }
+    public void deleteCarById(String carId) {
+
+        String SQL = "DELETE CAR WHERE ID = ?";
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setString(1, carId);
+            preparedStatement.execute();
+
+            System.out.println("success on delete car with id: " + carId);
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+        }
+
+    }
 
     public List<Car> findAllCars() {
         String SQL = "SELECT * FROM CAR";
@@ -53,9 +78,10 @@ public class CarDao {
 
             while (resultSet.next()) {
 
+                String carId = resultSet.getString("id");
                 String carName = resultSet.getString("name");
 
-                Car car = new Car(carName);
+                Car car = new Car(carId, carName);
 
                 cars.add(car);
 
